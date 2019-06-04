fc496f4288799e3c82b77b007acfa5202d0d2264
Laney McGlohon <lmcglohon@gmail.com>
2019-06-03 17:47:00 -0700

    Update to Rails 5.2.3

--- frontend/config/environments/production.rb
+++ frontend/config/environments/production.rb
@@ -70,7 +70,8 @@ ArchivesSpace::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
+  # HEADS UP! i18n 1.1 changed fallbacks to exclude default locale.
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
